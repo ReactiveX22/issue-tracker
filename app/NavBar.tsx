267668732716cf5336b334b396ab86f7d32655ec
ReactiveX22 <akibreactive@gmail.com>
2024-04-14 17:15:01 +0600

    auth: secure routes

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -12,6 +12,7 @@ import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { BsBugFill } from 'react-icons/bs';
+import { Skeleton } from '@/app/components';
 
 interface Links {
   label: string;
@@ -19,14 +20,6 @@ interface Links {
 }
 
 const NavBar = () => {
-  const links: Links[] = [
-    { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: '/issues' },
-  ];
-
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
   return (
     <nav className='mb-5 border-b px-5 py-3'>
       <Container>
@@ -35,51 +28,75 @@ const NavBar = () => {
             <Link href='/'>
               <BsBugFill />
             </Link>
-            <ul className='flex space-x-6'>
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={cn(
-                      'text-zinc-500 transition-colors hover:text-zinc-700',
-                      { 'text-zinc-900': link.href === currentPath }
-                    )}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === 'authenticated' && (
-              <DropdownMenu.Root dir='rtl'>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    className='cursor-pointer'
-                    src={session.user!.image!}
-                    fallback='?'
-                    size='2'
-                    radius='full'
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size='2'>{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href='/api/auth/signout'>Signout</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === 'unauthenticated' && (
-              <Link href='/api/auth/signin'>Sign In</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const links: Links[] = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
+
+  const currentPath = usePathname();
+
+  return (
+    <ul className='flex space-x-6'>
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={cn('nav-link', {
+              'text-zinc-900': link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === 'loading') return <Skeleton width='3rem' />;
+
+  if (status === 'unauthenticated')
+    return (
+      <Link className='nav-link' href='/api/auth/signin'>
+        Sign In
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root dir='rtl'>
+        <DropdownMenu.Trigger>
+          <Avatar
+            className='cursor-pointer'
+            src={session!.user!.image!}
+            fallback='?'
+            size='2'
+            radius='full'
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size='2'>{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href='/api/auth/signout'>Signout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
---
+++ app/auth/authOptions.ts
@@ -0,0 +1,26 @@
+import NextAuth, { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    session({ session, token, user }) {
+      return session; // The return type will match the one returned in `useSession()`
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+};
+
+export default authOptions;
