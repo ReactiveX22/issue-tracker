307de4bbad8adc1d4aa11320aecd14f46c3698c8
ReactiveX22 <akibreactive@gmail.com>
2024-04-18 17:06:00 +0600

    feat: improved sign in and sign out options

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,18 +1,25 @@
 'use client';
+import { Skeleton } from '@/app/components';
 import { cn } from '@/utils/cn';
 import {
   Avatar,
   Box,
+  Button,
   Container,
+  Dialog,
   DropdownMenu,
   Flex,
+  Spinner,
   Text,
 } from '@radix-ui/themes';
-import { useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { BiLogOut } from 'react-icons/bi';
 import { BsBugFill } from 'react-icons/bs';
-import { Skeleton } from '@/app/components';
+import { FaGoogle } from 'react-icons/fa';
 
 interface Links {
   label: string;
@@ -64,18 +71,59 @@ const NavLinks = () => {
 };
 
 const AuthStatus = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const { status, data: session } = useSession();
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      await signIn('google');
+    } catch (error) {
+      toast.error('Something went wrong with your login');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const logoutWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      await signOut();
+    } catch (error) {
+      toast.error('Something went wrong with your login');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   if (status === 'loading') return <Skeleton width='3rem' />;
 
   if (status === 'unauthenticated')
     return (
-      <Link
-        className='nav-link font-medium text-[var(--accent-9)]'
-        href='/api/auth/signin'
-      >
-        Sign In
-      </Link>
+      <Dialog.Root>
+        <Dialog.Trigger>
+          <Button>Sign In</Button>
+        </Dialog.Trigger>
+
+        <Dialog.Content maxWidth='450px'>
+          <Dialog.Title>Sign In</Dialog.Title>
+          <Dialog.Description size='2' mb='4'>
+            Sign in with Google to get access.
+          </Dialog.Description>
+
+          <Flex gap='3' mt='4' justify='end'>
+            <Dialog.Close>
+              <Button variant='soft' color='gray'>
+                Cancel
+              </Button>
+            </Dialog.Close>
+
+            <Button onClick={() => loginWithGoogle()} disabled={isLoading}>
+              {isLoading ? <Spinner /> : <FaGoogle />} Sign In
+            </Button>
+          </Flex>
+        </Dialog.Content>
+      </Dialog.Root>
     );
 
   return (
@@ -95,8 +143,13 @@ const AuthStatus = () => {
             <Text size='2'>{session!.user!.email}</Text>
           </DropdownMenu.Label>
           <Link href='/api/auth/signout'>
-            <DropdownMenu.Item className='cursor-pointer'>
-              Signout
+            <DropdownMenu.Item
+              className='cursor-pointer'
+              disabled={isLoading}
+              onClick={() => logoutWithGoogle()}
+            >
+              {isLoading && <Spinner />}
+              <BiLogOut size={20} /> Log Out
             </DropdownMenu.Item>
           </Link>
         </DropdownMenu.Content>
